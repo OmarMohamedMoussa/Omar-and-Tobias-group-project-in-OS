3a584bdeb5923ac8ff0363cbb902bcc688946d7b
Omar <omarmmo@stud.ntnu.no>
2023-02-26 20:15:21 +0100

    Task one done in movie ticket server with comments

--- src/main/java/booking/MovieTicketServer.java
+++ src/main/java/booking/MovieTicketServer.java
@@ -1,14 +1,18 @@
 package booking;
 
 public class MovieTicketServer {
+
+    // Instance variables to store the movie name and number of available seats
     private String movieName;
     private int availableSeats;
 
+    // Constructor to initialize the instance variables
     public MovieTicketServer(String movieName, int availableSeats){
         this.movieName = movieName;
         this.availableSeats = availableSeats;
     }
 
+    // Method to book tickets
     public void bookTicket(String customerName, int numberOfSeats){
         System.out.println("Hi," + customerName+ ": " + availableSeats + "Seats available for" + movieName);
 
